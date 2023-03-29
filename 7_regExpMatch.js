7c4724e7b2f4cb2c2a51c4945e15325039595547
ARGISHTI SARHATYAN <sarhatyan2001@gmail.com>
2023-03-29 17:45:50 +0400

    writed reverseStrin function

--- 7_regExpMatch.js
+++
@@ -1,6 +0,0 @@
-function changeSentence(string) {
-    const regExp = /<[^<>]+>/g;
-    return string.match(regExp);
-  };
-  let str = '<> <a href="/"> <input type="radio" checked> <b>';
-  console.log(changeSentence(str));
--- 8_dayOfYearFunction.js
+++
@@ -1,7 +0,0 @@
-function dayOfYear(date){
-    const currdate = new Date(date)
-    const firstDayOfYear = new Date(`01/01/${currdate.getFullYear()}`)
-    
-    return (currdate - firstDayOfYear) /( 24 * 3600 *1000) + 1
-}
-console.log(dayOfYear("1/2/2019"));
