03af24ab61bc5935fdf87492138c774b52718fa1
ARGISHTI SARHATYAN <sarhatyan2001@gmail.com>
2023-03-29 17:50:59 +0400

    writed regExpMatch function

---
+++ 7_regExpMatch.js
@@ -0,0 +1,6 @@
+function changeSentence(string) {
+    const regExp = /<[^<>]+>/g;
+    return string.match(regExp);
+  };
+  let str = '<> <a href="/"> <input type="radio" checked> <b>';
+  console.log(changeSentence(str));
